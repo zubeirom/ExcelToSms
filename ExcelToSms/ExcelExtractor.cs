07c773d566667800e50613a3af8ee79bc99b54c4
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-12 04:55:00 +0200

    add interface and classes

---
+++ ExcelToSms/ExcelExtractor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelToSms
+{
+    class ExcelExtractor : IExtractor
+    {
+        public List<T> extract<T>(string filePath)
+        {
+
+        }
+    }
+}
---
+++ ExcelToSms/IExtractor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelToSms
+{
+    public interface IExtractor
+    {
+        List<T> extract<T>(string filePath);
+    }
+}
